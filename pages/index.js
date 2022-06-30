39b82862c1d724cd7e8075f004a18d14083d4718
Stijn Rogiest <reddusted@gmail.com>
2022-06-30 19:16:11 +0200

    Use Link instead of anchor to satify compiler

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div>
-            This website will return the current Google doodle image at <a href="/api/doodle">/api/doodle</a>
+            This website will return the current Google doodle image at <Link href="/api/doodle">/api/doodle</Link>
             <img src="/api/doodle" />
         </div>
     );
